51a9554f2b089a1c3be957fde203aa4cf742fdfd
ilhamsangaji <ilhamsangaji.jobs@gmail.com>
2024-05-15 08:19:39 +0700

    update

--- javapos/src/main/java/com/xsis/javapos/apicontrollers/SalamController.java
+++ javapos/src/main/java/com/xsis/javapos/apicontrollers/SalamController.java
@@ -2,20 +2,22 @@ package com.xsis.javapos.apicontrollers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xsis.javapos.models.Salam;
 
 @RestController
+@RequestMapping("api/Salam")
 public class SalamController {
-	@GetMapping("api//Salam/{nama}")
-	public String Salam(@PathVariable final String nama) {
-		return "Selamat Datang, " + nama + "!";
-	}
-	
-	@GetMapping("api/Salam")
+	@GetMapping("")
 	public Salam SalamJuga(@RequestParam(value = "nama", defaultValue = "Orang") final String nama) {
 		return new Salam (13 , nama);
+	}
+	
+	@GetMapping("/{nama}")
+	public String Salam(@PathVariable final String nama) {
+		return "Selamat Datang, " + nama + "!";
 	}
 }
