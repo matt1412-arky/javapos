920f582ee11d6720b853c864872ec5db42cc692b
ilhamsangaji <ilhamsangaji.jobs@gmail.com>
2024-05-15 09:54:36 +0700

    update

---
+++ javapos/src/main/java/com/xsis/javapos/controllers/CustomerController.java
@@ -0,0 +1,16 @@
+package com.xsis.javapos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    @GetMapping("")
+	ModelAndView Index() {
+		return new ModelAndView("/customer/index");
+	}
+}
